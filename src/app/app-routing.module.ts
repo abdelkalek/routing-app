3acda44cc61242f3b8a55dca290963d1bfc9c51c
abdelkalek <abdelkalek@gmail.com>
2021-04-20 10:54:38 +0100

    third commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent} from './first/first.component';
+import {AppComponent} from './app.component';
+import {SecondComponent} from './second/second.component';
+import {ThirdComponent} from './third/third.component';
+import {MenuComponent} from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MenuComponent },
+  {path: 'first-component', component: FirstComponent },
+  { path: 'second-component', component: SecondComponent },
+  { path: 'third-component', component: ThirdComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
